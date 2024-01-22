840c32742f7bf7ead8dc82ea34389b7ed8d19f58
QuantumSaba <tmatt0209@gmail.com>
2024-01-22 02:12:27 -0500

    Dual Blade

--- scripts/npc/2007.js
+++ scripts/npc/2007.js
@@ -1,34 +1,30 @@
 var status = -1;
 
-/*
 function start() {
-    cm.warp(104000000);
-    cm.sendPlayerToNpc("Ah, finally here!");
-    cm.dispose();
+	if (cm.getPlayer().getSubcategory() == 1) {
+		cm.sendOk("Begin your journey!");
+		cm.dispose();
+		return;
+		}
+	if (cm.getPlayer().getMap().getId() != 10000 || cm.getPlayer().getLevel() > 1) {
+		cm.sendOk("Begin your journey!");
+		cm.dispose();
+		return;
+		}
+		var chat = "Choose your path as an Explorer, Normal or Dual Blade?";
+		chat += "\r\n#L0#Normal Explorer";
+		chat += "\r\n#L1#Dual Blade";
+		cm.sendSimple(chat);
 }
-*/
 
-/*
 function action(mode, type, selection) {
-    if (cm.getPlayer().getLevel() != 1 || cm.getPlayer().getMapId() != 10000) {
-	cm.dispose();
-	return;
-    }
-    if (mode == 1) {
-	status++;
-    } else {
-	if (status == 0) {
-	    cm.dispose();
-	}
-	status--;
-    }
-    if (status == 0) {
-		cm.sendSimple("#b#L3#Head to Lith Harbor#l\r\n#L4#Stay in Maple Island#l");
-    } else if (status == 1) {
-	if (selection == 3) {
-	    cm.warp(104000000);
-	}
-	cm.dispose();
-    }
+	switch (selection) {
+	case 0:
+		cm.sendOk("Begin your journey!");
+		break;
+	case 1:
+		cm.getPlayer().setSubcategory(1);
+		cm.getPlayer().fakeRelog();
+		}
+		cm.dispose();
 }
-*/
