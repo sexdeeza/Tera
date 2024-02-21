594e0c060c081515177ad59983e7beda4441d1dd
QuantumSaba <tmatt0209@gmail.com>
2024-02-21 01:27:55 -0500

    Super Miracle Cube Text

--- scripts/item/2430481.js
+++ scripts/item/2430481.js
@@ -6,13 +6,13 @@
 
 function start() {
 	if (im.getPlayer().itemQuantity(2430481) < 10) {
-		im.getClient().getSession().write(Packages.tools.packet.CWvsContext.serverNotice(6, "收集10個傳說的奇幻方塊碎片，可以製作一個傳說方塊"));
-		im.dispose();
-		return;
-		}
-	if (im.getPlayer().getInventory(Packages.client.inventory.MapleInventoryType.CASH).getNumFreeSlot() < 1) {
-		im.getClient().getSession().write(Packages.tools.packet.CWvsContext.serverNotice(6, "製作傳說方塊之前，請在特殊欄保留一個空位"));
-		im.dispose();
+	im.getClient().getSession().write(Packages.tools.packet.CWvsContext.serverNotice(6, "Collect 10 Super Miracle Cube fragments to make a Super Miracle Cube"));
+    im.dispose();
+    return;
+    }
+    if (im.getPlayer().getInventory(Packages.client.inventory.MapleInventoryType.CASH).getNumFreeSlot() < 1) {
+    im.getClient().getSession().write(Packages.tools.packet.CWvsContext.serverNotice(6, "Before making the Super Miracle Cube, please leave an empty space in the cash inventory"));
+    im.dispose();
 		return;
 		}
 		im.gainItem(2430481, -10);
